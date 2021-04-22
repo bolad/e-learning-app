8de4a1434722955569735d86908612fb3ffb8ad0
stanley akyea <stanley.akyea@gmail.com>
2021-04-22 16:17:01 +0200

    Add more fields to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'stanley@gmail.com', password: 'password', password_confirmation: 'password')
+#User.create!(email: 'stanley@gmail.com', password: 'password', password_confirmation: 'password')
 
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::GameOfThrones.quote,
-    user_id: User.first.id
+    user_id: User.first.id,
+    short_description: Faker::Quote.famous_last_words,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 100, to: 300)
   }])
 end
