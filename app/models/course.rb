83f50b5ef079a7a59306d770c4b683f771ef8ebd
stanley akyea <stanley.akyea@gmail.com>
2021-04-25 17:51:54 +0200

    Change search field in header to use ransack and add public_activity gem to track all actions in the app

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,4 +19,16 @@ class Course < ApplicationRecord
   #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
   # end
 
+  LANGUAGES = [:"English", :"French", :"Polish"]
+  def self.languages
+    LANGUAGES.map{ |language| [language, language]}
+  end
+
+  LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
+  def self.levels
+    LEVELS.map{ |level| [level, level]}
+  end
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 end
