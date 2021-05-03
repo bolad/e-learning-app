7c134284b0b58a7866335a5c44374858314bf722
stanley akyea <stanley.akyea@gmail.com>
2021-05-03 19:41:19 +0200

    Add lessons to a course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,7 +16,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1 or /courses/1.json
   def show
-
+    @lessons = @course.lessons
   end
 
   # GET /courses/new
@@ -66,7 +66,7 @@ class CoursesController < ApplicationController
   # DELETE /courses/1 or /courses/1.json
   def destroy
     authorize @course
-    
+
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
