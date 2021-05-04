798874fb369a1a6b01acd8e13f31c6275253a7fd
stanley akyea <stanley.akyea@gmail.com>
2021-05-04 21:36:34 +0200

    Change access method for sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => Rails.application.credentials.sendgrid[:user_name],
-    :password => Rails.application.credentials.sendgrid[:password],
+    :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
+    :password => Rails.application.credentials.dig(:sendgrid, password),
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
